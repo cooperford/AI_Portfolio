036e5acc36f82b1128f51bd4363e9dd68a2e94a5
cooperford <cooper.ford0031@gmail.com>
2026-05-07 14:22:48 -0500

    Fix static assets on Render

--- config/settings.py
+++ config/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
